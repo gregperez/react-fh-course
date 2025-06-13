b15bdbeea3ebe259381ca7a44e5fc7abd54b0959
Greg Perez <gperez@cleveritgroup.com>
2025-06-13 15:30:27 -0400

    fix: renombrar carpetas modulo 04

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,5 +1,5 @@
 
-export const GiftExpertApp = () => {
+export const GifExpertApp = () => {
   return (
     <>
         <h1>GiftExpert</h1>  
--- 04-gif-expert-app/src/main.jsx
+++ 04-gif-expert-app/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { GiftExpertApp } from './GiftExpertApp'
+import { GifExpertApp } from './GifExpertApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GiftExpertApp />
+    <GifExpertApp />
   </StrictMode>,
 )
