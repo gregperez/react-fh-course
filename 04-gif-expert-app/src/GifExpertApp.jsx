7ab530f3b0bd9db9a876b9ea33b1e8f5760e28ad
Greg Perez <gperez@cleveritgroup.com>
2025-06-13 15:59:58 -0400

    creando una lista de categorias

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  console.log(categories);
+
   return (
     <>
-        <h1>GiftExpert</h1>  
+        <h1>GiftExpert</h1>
+
+        <ol>
+          { categories.map( category => {
+              return <li key={ category }>{ category }</li>;
+            })
+          }
+        </ol>
     </>
   );
 };
