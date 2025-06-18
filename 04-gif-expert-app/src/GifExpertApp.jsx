50c3bfe910d4734e1a70ebfa260408b23159c371
Greg Perez <gperez@cleveritgroup.com>
2025-06-17 23:04:45 -0400

    agregando una nueva categoria y css para botón

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -4,11 +4,16 @@ export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
-  console.log(categories);
+  const onAddCategory = () => {
+    // categories.push('Valorant');
+    setCategories([ ...categories, 'Valorant' ]);
+  };
 
   return (
     <>
         <h1>GiftExpert</h1>
+
+        <button onClick={ onAddCategory }>Agregar</button>
 
         <ol>
           { categories.map( category => {
