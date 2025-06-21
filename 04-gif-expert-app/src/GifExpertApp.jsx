cb9ca58aaf64c2750d68b535bddfef58554b2749
Greg Perez <gperez@cleveritgroup.com>
2025-06-20 20:23:58 -0400

    nuevo componente GifGrid

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
@@ -27,9 +28,12 @@ export const GifExpertApp = () => {
         {/* <button onClick={ onAddCategory }>Agregar</button> */}
 
         <ol>
-          { categories.map( category => {
-              return <li key={ category }>{ category }</li>;
-            })
+          { 
+            categories.map( ( category ) => (
+              <GifGrid
+                key={ category }
+                category={ category } />
+            ))
           }
         </ol>
     </>
