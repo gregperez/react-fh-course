938be48320588690786a5ee3de533de6e762f4d5
Greg Perez <gperez@cleveritgroup.com>
2025-06-20 20:01:31 -0400

    validar nombres unicos en array

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -6,7 +6,7 @@ export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
   const onAddCategory = (newCategory) => {
-    // console.log(newCategory);
+    if (categories.includes(newCategory)) return;
 
     // categories.push(category);
     setCategories([ newCategory, ...categories ]);
