51d1b519ef382e38b9e92cc12702eeafe6f646fa
Greg Perez <gperez@cleveritgroup.com>
2025-07-06 18:31:09 -0400

    new gifs by default

--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async( category )  => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=BCp0ER1sfov5jmxSFqhnWzl2GQvXer8k&q=${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=theKey&q=${category}&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
