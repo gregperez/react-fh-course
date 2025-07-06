dcef7d33125c2511110f5d012b7f4cec95ba7110
Greg Perez <gperez@cleveritgroup.com>
2025-07-05 22:44:53 -0400

    mostrar los titulos de las imagenes

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,16 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
+
+    const [images, setImages] = useState([]);
     
+    const getImages = async () => {
+        const newImagens = await getGifs( category );
+        setImages(newImagens);
+    };
+
     useEffect( () => {
-        getGifs(category);
-    }, [ ]);
+        getImages();
+    }, []);
 
 
     return (
         <>
             <h3>{ category }</h3>
+
+            <ol>
+                { 
+                    images.map( ({ id, title }) => (
+                    <li key={ id }>{ title }</li>
+                    ))
+                }
+            </ol>
         </>
     )
 }
